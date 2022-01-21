d5e59b5d898a4ed12fc0064c21e9c79dd7f6b942
Nathan Nguyen <nathantoannguyen@gmail.com>
2022-01-20 18:12:23 -0800

    implemented onClick, shows title, rating, and overview

---
+++ app/src/main/java/com/example/flixster/DetailActivity.kt
@@ -0,0 +1,30 @@
+package com.example.flixster
+
+import android.media.Rating
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
+import org.w3c.dom.Text
+
+private const val TAG = "DetailActivity"
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvOverview: TextView
+    private lateinit var ratingBar: RatingBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvOverview = findViewById(R.id.tvOverview)
+        ratingBar = findViewById(R.id.rbVoteAverage)
+
+        val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
+        tvTitle.text = movie.title
+        tvOverview.text = movie.overview
+        ratingBar.rating = movie.voteAverage.toFloat()
+
+    }
+}
